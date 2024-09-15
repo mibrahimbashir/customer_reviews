21a4c1d85814fd53e346a16ed5320f74c7e21ddf
ibrahim bashir <ibrahimbashir370@gmail.com>
2024-09-15 12:13:23 +0500

    Crawl business pages and collect customer reviews

--- customer_reviews/customer_reviews/settings.py
+++ customer_reviews/customer_reviews/settings.py
@@ -1,12 +1,3 @@
-# Scrapy settings for customer_reviews project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     https://docs.scrapy.org/en/latest/topics/settings.html
-#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-
 BOT_NAME = "customer_reviews"
 
 SPIDER_MODULES = ["customer_reviews.spiders"]
@@ -17,7 +8,7 @@ NEWSPIDER_MODULE = "customer_reviews.spiders"
 #USER_AGENT = "customer_reviews (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- customer_reviews/customer_reviews/spiders/tp.py
+++ customer_reviews/customer_reviews/spiders/tp.py
@@ -7,4 +7,53 @@ class TpSpider(scrapy.Spider):
     start_urls = ["https://www.trustpilot.com/"]
 
     def parse(self, response):
-        pass
+        category_page_url = response.xpath("//header/div/a[2]/@href").get()
+
+        category_page_url = "https://www.trustpilot.com/" + category_page_url
+        
+        yield response.follow(category_page_url, callback=self.parse_all_categories_page)
+    
+    def parse_all_categories_page(self, response):
+
+        categories_url = response.css("main li a::attr(href)").getall()
+
+        for category_url in categories_url:
+            category_url = "https://www.trustpilot.com/" + category_url
+
+            yield response.follow(category_url, callback=self.parse_category_page)
+    
+    def parse_category_page(self, response):
+        businesses_url = response.css('div.styles_wrapper__2JOo2 a::attr(href)').getall()
+
+        for business_url in businesses_url:
+            business_url = "https://www.trustpilot.com/" + business_url
+            yield response.follow(business_url, callback=self.parse_business_page)
+        
+        next_page_url = response.xpath('//nav[last()]/a[last()]/@href').get(default='')
+
+        if next_page_url:
+            next_page_url = "https://www.trustpilot.com" + next_page_url
+            yield response.follow(next_page_url, callback=self.parse_category_page)
+    
+    def parse_business_page(self, response):
+        business_unit = response.css("#business-unit-title")
+
+        business_name = business_unit.css("h1 span::text").get()
+        business_category = business_unit.xpath(".//a[last()]/text()").get(default='')
+
+        customer_reviews = response.css("article")
+
+        for review in customer_reviews:
+            yield {
+                "rating" : review.css('div.styles_reviewHeader__iU9Px::attr(data-service-review-rating)').get(default='0'),
+                "review_date" : review.xpath('.//time[1]/@datetime').get(default=''),
+                "title" : review.css('h2::text').get(default=''),
+                "description" : review.css('.typography_body-l__KUYFJ.typography_appearance-default__AAY17.typography_color-black__5LYEn::text').get(default=''),
+                "date_experience" : review.css('p.typography_body-m__xgxZ_.typography_appearance-default__AAY17::text').getall()[-1]
+            }
+
+        next_page_url = response.xpath('//nav[last()]/a[last()]/@href').get(default='')
+
+        if next_page_url:
+            next_page_url = "https://www.trustpilot.com" + next_page_url
+            yield response.follow(next_page_url, callback=self.parse_business_page)
