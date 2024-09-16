1da85186cfd88f7df621881fbc70975a17768239
ibrahim bashir <ibrahimbashir370@gmail.com>
2024-09-16 13:37:08 +0500

    Modify settings to add delay in each request. Add requirements.txt

--- customer_reviews/customer_reviews/settings.py
+++ customer_reviews/customer_reviews/settings.py
@@ -3,17 +3,17 @@ BOT_NAME = "customer_reviews"
 SPIDER_MODULES = ["customer_reviews.spiders"]
 NEWSPIDER_MODULE = "customer_reviews.spiders"
 
-# FEEDS = {
-# 	"reviews.csv" : {
-# 		"format" : "csv",
-# 		"overwrite" : True
-# 	},
+FEEDS = {
+	"reviews.csv" : {
+		"format" : "csv",
+		"overwrite" : True
+	},
 
-# 	"reviews.json" : {
-# 		"format" : "json",
-# 		"overwrite" : True
-# 	}
-# }
+	"reviews.json" : {
+		"format" : "json",
+		"overwrite" : True
+	}
+}
 
 SCRAPEOPS_BROWSER_HEADERS_ENDPOINT = "https://headers.scrapeops.io/v1/browser-headers"
 SCRAPEOPS_BROWSER_HEADERS_ACTIVE = True
@@ -23,12 +23,12 @@ SCRAPEOPS_BROWSER_HEADERS_NUM_RESULTS = 129
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 16
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -72,14 +72,14 @@ ITEM_PIPELINES = {
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_START_DELAY = 3
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_MAX_DELAY = 240
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
 #AUTOTHROTTLE_DEBUG = False
 
